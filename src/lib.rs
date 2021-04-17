3082b235aa2ffb759ba5633c1a3cbd11155f1e43
watcol <potfman@gmail.com>
2021-04-17 14:15:13 +0900

    Add common interface.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,25 @@
 //! Simple CLI Parser for Data Formats
+use std::collections::HashMap;
+
+/// Represents the Parsed value.
+#[derive(Clone, Debug)]
+pub enum Value {
+    String(String),
+    Int(i64),
+    Float(f64),
+    Boolean(bool),
+    Array(Vec<Value>),
+    Map(HashMap<String, Value>)
+}
+
+/// The unified interface for parsing data format.
+pub trait Parser {
+    type Err;
+    fn parse(s: &str) -> Result<Value, Self::Err>;
+}
+
+/// The unified interface for generate data format.
+pub trait Generator {
+    type Err;
+    fn generate(value: &Value) -> Result<String, Self::Err>;
+}
