cb6db13721669cc0ed68703dcc97725686170edd
watcol <potfman@gmail.com>
2021-04-18 16:55:29 +0900

    Add minimal json generator.

--- src/bin/jgen.rs
+++ src/bin/jgen.rs
@@ -1,10 +1,13 @@
 //! JSON Generator
-use df_utils::{Value, PrintConfig};
-use std::io::{Read, stdin};
+use df_utils::{Value, PrintConfig, Generator, MinJsonGenerator};
+use std::io::{Read, stdin, stdout};
 
 fn main() {
     let mut s = String::new();
     stdin().read_to_string(&mut s).expect("Failed to read");
-    println!("{}", s);
-    println!("{:?}", Value::parse(&s, &PrintConfig::new()));
+    let value = Value::parse(&s, &PrintConfig::new()).unwrap_or_else(|e| {
+        println!("{}", e);
+        std::process::exit(1);
+    });
+    MinJsonGenerator::generate(&mut stdout(), &value).expect("Failed to write");
 }
--- src/generator/min_json.rs
+++ src/generator/min_json.rs
@@ -11,5 +11,31 @@ impl Generator for MinJsonGenerator {
 }
 
 fn inner_generate<W: Write>(buf: &mut W, value: &Value) -> io::Result<()> {
+    match value {
+        Value::Null => write!(buf, "null")?,
+        Value::Boolean(b) => write!(buf, "{}", b)?,
+        Value::Int(i) => write!(buf, "{}", i)?,
+        Value::Float(f) if *f == f64::INFINITY => write!(buf, "Infinity")?,
+        Value::Float(f) if *f == f64::NEG_INFINITY => write!(buf, "-Infinity")?,
+        Value::Float(f) => write!(buf, "{}", f)?,
+        Value::String(s) => write!(buf, "{:?}", s)?,
+        Value::Array(vs) => {
+            write!(buf, "[")?;
+            for (i, v) in vs.iter().enumerate() {
+                if i != 0 { write!(buf, ",")?; }
+                inner_generate(buf, v)?;
+            }
+            write!(buf, "]")?;
+        }
+        Value::Map(m) => {
+            write!(buf, "{{")?;
+            for (i, (k, v)) in m.iter().enumerate() {
+                if i != 0 { write!(buf, ",")?; }
+                write!(buf, "{:?}:", k)?;
+                inner_generate(buf, v)?;
+            }
+            write!(buf, "}}")?;
+        }
+    }
     Ok(())
 }
